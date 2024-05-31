43de48ae65b3916e671c8ca9ea82e06094aed37b
Scott Fennell <slfennell01@gmail.com>
2024-05-31 15:39:37 -0400

    Instantiate Manager objects and calculate annual salaries with bonuses
    Instantiate Manager objects and calculate annual salaries with bonuses. Fixed errors within code.

--- cc_4.js
+++ cc_4.js
@@ -4,23 +4,31 @@ class Employee {
     constructor(name, salary){
         this.name = name;
         this.salary = salary;
-        console.log(`Employee ${this.name}, has a montly salary of $${this.salary}`);//console info
+        console.log(`Employee ${this.name}, has a monthly salary of $${this.salary}.`);//console info
     }
     calcAnnualSalary(){
         return this.salary * 12; //method for calculating yearly
     }
 }
+
 //2.) Manager Subclass. Add department property. Override calcAnnualSalary. Bonus. Console.
 class Manager extends Employee{
-    constructor(name, salary,department){
+    constructor(name, salary, department){
         super(name, salary);
         this.department = department;
     }
     calcAnnualSalary(){
-        let bonus = this.bonus * 0.15;
+        let bonus = this.salary * 0.15;
         let Salary_Bonus = super.calcAnnualSalary() + bonus;
-        console.log(`Manager ${this.name}, works in the ${department} has a bonus of $${bonus} and total annual salary including bonus of $${Salary_Bonus}}`);
+        console.log(`Manager ${this.name}, works in the ${this.department} has a bonus of $${bonus} and total annual salary including bonus of $${Salary_Bonus}.`);
         return Salary_Bonus;
     }
-
 }
+
+//3.) Instantiate Manager Objects. Create two managers. Calculate and log data.
+const manager_BR = new Manager("Bob Ross", 6600.00, "Digital Marketing"); //test data 1
+const manager_DL = new Manager("Debbie Little", 7205.00, "Finance"); //test data 2
+
+console.log("Managers Annual Salary:");
+console.log(manager_BR.calcAnnualSalary());
+console.log(manager_DL.calcAnnualSalary());
